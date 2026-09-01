965222ad5aa9dbe76a5d158a68cf17da02f62e59
gamezluis228-maker <gamezluis228@gmail.com>
2026-08-31 22:19:05 -0400

    Add files via upload

--- bot_render.py
+++ bot_render.py
@@ -18,8 +18,8 @@ def run_web_server():
 
 threading.Thread(target=run_web_server, daemon=True).start()
 
-# Token real y completo de tu bot
-TOKEN = 8962151587:AAFZkPd73TnVDS_PZVfejFPGb1U_pbdPr1E
+# Token real y completo de tu bot entre comillas
+TOKEN = "8962151587:AAFZkPd73TnVDS_PZVfejFPGb1U_pbdPr1E"
 
 
 application = Application.builder().token(TOKEN).build()
